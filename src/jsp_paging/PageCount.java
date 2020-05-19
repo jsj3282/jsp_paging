787e05ecd369781c6f8b7d56255687a73f85a5ca
jsj <jeongseonju15@gmail.com>
2020-05-19 21:29:22 +0900

    jsp 페이지 나누기

---
+++ src/jsp_paging/PageCount.java
@@ -0,0 +1,27 @@
+package jsp_paging;
+
+public class PageCount {
+	private int startPage;
+	private int endPage;
+	private int totalPage;
+	
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	public int getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+	
+}	
